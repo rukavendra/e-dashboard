f1a8502da8d8fe8ae77ec3f4ae9afc4a9062315c
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-09-21 21:22:01 +0530

    Update workoutDetails.jsx
    delete and update button added with handler functions

--- frontend/src/components/workoutDetails/workoutDetails.jsx
+++ frontend/src/components/workoutDetails/workoutDetails.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { useWorkoutsContext } from '../../context/hooks/useWorkoutsContext'
 
 const WorkoutDetails = ({workout}) => {
-  return (
-    <div className='flex flex-col my-2 mx-auto p-2 bg-white w-[90%] md:w-[70%] '>
+  const {dispatch}= useWorkoutsContext()
+
+  const handleDelete= async()=>{
+    const response = await fetch(`/workouts/${workout._id}`,{method:"DELETE"})
+    const json = await response.json()
+    if(response.ok){
+      dispatch({type:'DELETE_WORKOUT',payload:json})
+    }
+  }
+
+  const handleUpdate= ()=>{}
+
+  return (<div className='flex justify-between items-center my-2 mx-auto p-2 bg-white w-[90%] md:w-[70%]'>
+    <div className='flex flex-col  '>
     <h3 className='font-bold text-xl text-green-500'>{workout.title}</h3>
     <h4><strong>Reps: </strong>{workout.reps}</h4>
     <h5><strong>Weight (Kg) :</strong> {workout.load} </h5>
     <p>{workout.createdAt}</p>
+</div>
+<div className='flex flex-col gap-1'>
+<button className=' hover:bg-black hover:bg-opacity-50 py-1 px-2 rounded' onClick={handleDelete}>❌</button>
+  <button className=' hover:bg-black hover:bg-opacity-50 py-1 px-2 rounded' onClick={handleUpdate}>✒️</button>
+</div>
+  
 </div>
   )
 }

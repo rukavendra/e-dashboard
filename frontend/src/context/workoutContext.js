e3c05e4a5ba56a6362d209f864e29b7d34e60fb0
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-09-21 21:22:40 +0530

    Update workoutContext.js
    delete action added

--- frontend/src/context/workoutContext.js
+++ frontend/src/context/workoutContext.js
@@ -10,6 +10,9 @@ export const workoutReducer = (state,action)=>{
         case 'CREATE_WORKOUT':
             return {workouts: [action.payload,...state.workouts]}
 
+        case 'DELETE_WORKOUT':
+            return {workouts: state.workouts.filter(workout=>workout._id!==action.payload._id)}
+
         default:
             return state
     }

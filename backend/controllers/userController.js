753c25c972f2e49c5b12ad81e1eaf92f3113bf2d
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-09-23 22:05:12 +0530

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+const User = require('../models/useModel')
+
+
+// login
+
+const loginUser = async(req, res)=>{
+    res.json({message: 'Login'})
+}
+
+
+//signup
+
+const signUpUser = async(req,res)=>{
+    const {email, password}= req.body
+    try{
+        const user = await User.signup(email,password)
+        res.status(200).json({email,user})
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+    res.json({message: 'SignUp'})
+}
+
+
+module.exports = {loginUser, signUpUser}

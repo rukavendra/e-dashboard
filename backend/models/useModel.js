770cd980c67326592b3061886e693a21fe83a27c
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-09-28 22:43:33 +0530

    Update useModel.js
    statics login and signup method created

--- backend/models/useModel.js
+++ backend/models/useModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator= require('validator')
 
 const Schema = mongoose.Schema
 
@@ -15,7 +16,20 @@ const UserSchema = new Schema({
     }
 })
 
+// static signUp method 
 UserSchema.statics.signup = async function(email,password){
+
+    //validation 
+    if(!email || !password){
+        throw Error('All Fields Required')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Email is not Valid')
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password is not Strong Enough')
+    }
+    
     const exist = await this.findOne({email})
     if(exist){
         throw Error('Email Already in Use')
@@ -24,6 +38,25 @@ UserSchema.statics.signup = async function(email,password){
     const hash = await bcrypt.hash(password,salt)
 
     const user = await this.create({email, password:hash})
+
+    return user
+}
+
+// static login method
+UserSchema.statics.login = async function(email,password){
+    if(!email || !password){
+        throw Error('All Fields Required')
+    }
+
+    const user = await this.findOne({email})
+    if(!user){
+        throw Error('Invalid Email')
+    }
+
+    const match = await bcrypt.compare(password,user.password)
+    if(!match){
+        throw Error('Invalid Password')
+    }
 
     return user
 }

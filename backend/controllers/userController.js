7e84f04ec5184fe8105a4109db835177fbb90cb8
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-09-28 22:45:09 +0530

    Update userController.js
    login and signup method along with jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,20 @@
 const User = require('../models/useModel')
+const jwt = require('jsonwebtoken')
 
 
+const createToken = (_id)=>{
+    return jwt.sign({_id}, process.env.SECRET,{expiresIn: '3d'})}
 // login
 
 const loginUser = async(req, res)=>{
-    res.json({message: 'Login'})
+    const {email,password}= req.body
+    try{
+        const user = await User.login(email,password)
+        const token = createToken(user._id)
+        res.status(200).json({email, token})}
+    catch(error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 
@@ -14,7 +24,10 @@ const signUpUser = async(req,res)=>{
     const {email, password}= req.body
     try{
         const user = await User.signup(email,password)
-        res.status(200).json({email,user})
+        
+        // create a token
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
     }catch(error){
         res.status(400).json({error: error.message})
     }

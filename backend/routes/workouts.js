f43c70198146ab3f8255a42404b5c3e7e8df6b4c
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-10-05 22:08:34 +0530

    Update workouts.js

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const {createWorkout, deleteWorkout, getSingleWorkout, updateWorkout, getWorkouts}  = require('../controllers/workouts')
+const requireAuth = require('../middleware/authRequired')
 const router = express.Router()
+
+// middleware { required auth for all workout routes}
+router.use(requireAuth)
 
 router.route('/')
 .get(getWorkouts)

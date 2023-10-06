67e70c08686879a26af4c70330ef78904b5b0308
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-10-06 22:18:57 +0530

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import {createContext, useReducer} from 'react'
+import {createContext, useReducer, useEffect} from 'react'
 
 export const authReducer = (state, action)=>{
     switch(action.type){
@@ -14,6 +14,13 @@ export const authReducer = (state, action)=>{
 export const AuthContext = createContext()
 export const AuthContextProvider = ({children})=>{
     const [state, dispatch] = useReducer(authReducer,{user:null})
+    useEffect(()=>{
+        const user= JSON.parse(localStorage.getItem('user'))
+
+        if(user){
+            dispatch({type:'LOGIN', payload: user})
+        }
+    },[])
     return (<AuthContext.Provider value={{...state,dispatch}}>
        {children}
     </AuthContext.Provider>)
